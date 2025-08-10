9fa65a767ec715e34c5db6a1e90213b3bc8916a2
Shree Kanth <sshreekanth669@gmail.com>
2025-08-10 09:10:50 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { useEffect, useRef } from "react";
-import { useFormState, useFormStatus } from "react-dom";
+import { useEffect, useRef, useActionState } from "react";
+import { useFormStatus } from "react-dom";
 import { Button } from "@/components/ui/button";
 import { Input } from "../components/ui/input";
 import { Textarea } from "../components/ui/textarea";
@@ -29,7 +29,7 @@ const initialState = {
 export function Contact() {
   const { toast } = useToast();
   const formRef = useRef<HTMLFormElement>(null);
-  const [state, formAction] = useFormState(createContact, initialState);
+  const [state, formAction] = useActionState(createContact, initialState);
 
   useEffect(() => {
     if (state.message?.startsWith('Success:')) {
